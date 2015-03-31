4638fea1ef3940bffa024be279be4fb2dc25c8e0
Blaine Bublitz <blaine@iceddev.com>
2015-03-31 14:30:44 -0700

    implement bs2-serialport as a protocol instead of a serialport abstraction

--- index.js
+++ index.js
@@ -3,20 +3,63 @@
 var SerialPort = require('serialport').SerialPort;
 var when = require('when');
 var nodefn = require('when/node');
-var util = require('util');
 
-function BS2SP(path, options, openImmediately, callback){
-  SerialPort.call(this, path, options, openImmediately, callback);
-}
-util.inherits(BS2SP, SerialPort);
-
-
-BS2SP.prototype.reset = function reset(cb){
+function Protocol(options){
   var self = this;
+
+  this._serial = options.serialport || new SerialPort(options.path, options.options);
+
+  this._queue = null;
+
+  this._serial.on('data', function(chunk){
+    if(typeof self._queue === 'function'){
+      self._queue(chunk);
+    }
+  });
+}
+
+Protocol.prototype._onResponse = function(fn){
+  this._queue = fn;
+};
+
+Protocol.prototype.send = function send(data, cb){
+  var self = this;
+  var serial = this._serial;
+
+  var responseLength = data.length + 1;
+
+  var promise = when.promise(function(resolve, reject) {
+
+    var buffer = new Buffer(0);
+    function onChunk(chunk) {
+      buffer = Buffer.concat([buffer, chunk]);
+      if (buffer.length > responseLength) {
+        // or ignore after
+        return reject(new Error('buffer overflow ' + buffer.length + ' > ' + responseLength));
+      }
+      if (buffer.length === responseLength) {
+        resolve(buffer[data.length]);
+      }
+    }
+
+    self._onResponse(onChunk);
+
+    serial.write(data, function (writeError) {
+      if (writeError) {
+        return reject(writeError);
+      }
+    });
+  });
+
+  return nodefn.bindCallback(promise, cb);
+};
+
+Protocol.prototype.reset = function reset(cb){
+  var serial = this._serial;
 
   function setDtr(){
     return when.promise(function(resolve, reject) {
-      self.set({dtr: false}, function(err){
+      serial.set({dtr: false}, function(err){
         if(err){ return reject(err); }
         return resolve();
       });
@@ -25,7 +68,7 @@ BS2SP.prototype.reset = function reset(cb){
 
   function clrDtr(){
     return when.promise(function(resolve, reject) {
-      self.set({dtr: true}, function(err){
+      serial.set({dtr: true}, function(err){
         if(err){ return reject(err); }
         return resolve();
       });
@@ -34,7 +77,7 @@ BS2SP.prototype.reset = function reset(cb){
 
   function setBrk(){
     return when.promise(function(resolve, reject) {
-      self.write(new Buffer([0x00]), function(err){
+      serial.write(new Buffer([0x00]), function(err){
         if(err){ return reject(err); }
         return resolve();
       });
@@ -43,7 +86,7 @@ BS2SP.prototype.reset = function reset(cb){
 
   function clrBrk(){
     return when.promise(function(resolve, reject) {
-      self.update({baudRate: 9600}, function(err){
+      serial.update({baudRate: 9600}, function(err){
         if(err){ return reject(err); }
         return resolve();
       });
@@ -51,13 +94,26 @@ BS2SP.prototype.reset = function reset(cb){
   }
 
   var promise = setBrk()
-  .then(setDtr)
-  .delay(2)
-  .then(clrDtr)
-  .delay(100) //need to wait for the setbrk byte to get out on the line
-  .then(clrBrk);
+    .then(setDtr)
+    .delay(2)
+    .then(clrDtr)
+    .delay(100) //need to wait for the setbrk byte to get out on the line
+    .then(clrBrk);
 
   return nodefn.bindCallback(promise, cb);
 };
 
-module.exports = BS2SP;
+Protocol.prototype.signoff = function signoff(cb){
+  var serial = this._serial;
+
+  var promise = when.promise(function(resolve, reject) {
+    serial.write(new Buffer([0]), function(err){
+      if(err){ return reject(err); }
+      return resolve();
+    });
+  });
+
+  return nodefn.bindCallback(promise, cb);
+};
+
+module.exports = Protocol;
